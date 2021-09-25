d38279f1b4f1c77d39b614ef2a3eb6f607031b2a
vetero4ok <vetero4ok@gmail.com>
2021-09-25 17:48:28 +0300

    onClick OK and redirect to login

--- src/Componets/Error404/Error404.tsx
+++ src/Componets/Error404/Error404.tsx
@@ -1,21 +1,23 @@
 import {Button} from '@material-ui/core';
 import React from 'react';
-import {Redirect} from 'react-router-dom';
+import {useHistory} from 'react-router-dom';
 
 
-export const Error404:React.FC  = () => {
-    /** пофиксить редирект*/
+export const Error404: React.FC = () => {
+    const history = useHistory();
+
     const onClickHandler = () => {
-      return <Redirect  to = {'/login'}/>
+        let path = `/login`;
+        history.push(path);
     }
     return <>
         <h1 style={{textAlign: 'center', fontSize: '55px'}}>The page you're looking for can't be found! </h1>
         <h3 style={{textAlign: 'center'}}>Error 404</h3>
         <div style={{textAlign: 'center'}}>
             <Button
-            variant={'contained'}
-            color={'primary'}
-            onClick={onClickHandler}>Ok</Button>
+                variant={'contained'}
+                color={'primary'}
+                onClick={onClickHandler}>Ok</Button>
         </div>
     </>
 }
